fe6e67fd343b77edde705af45ee81c2f26be1d5e
Shaokun-X <shaokun.xie@outlook.fr>
2022-03-13 14:32:57 +0100

    implement explosion effect for projectile

--- Assets/Scripts/Core/BattleScene/Artifact/Projectile/LinearProjectileController.cs
+++ Assets/Scripts/Core/BattleScene/Artifact/Projectile/LinearProjectileController.cs
@@ -4,75 +4,96 @@ using UnityEngine;
 
 public class LinearProjectileController : MonoBehaviour
 {
-	[SerializeField] public ElementType ElementType = ElementType.Fire;
-	[SerializeField] public float Speed = 5f;
-	[SerializeField] public int Damage = 2;
-	[SerializeField] public bool HasNoPenetrateLlimit = false;
-	[SerializeField] public int PenetrateTime = 0;
 
-	[SerializeField] private ExplosionPos explosionPosition = ExplosionPos.Enemy;
+    public GameObject AreaEffectPrefab;
 
-	[SerializeField] private float autoDestructionTime = 10f;
+    [SerializeField] public ElementType ElementType = ElementType.Fire;
+    [SerializeField] public float Speed = 5f;
+    [SerializeField] public int CollisionDamage = 2;
+    [SerializeField] public bool HasPenetrateLlimit = false;
+    [SerializeField] public int PenetrateTime = 0;
 
-	public bool isAreaEffect = false;  
+    [SerializeField] private HitEffectTarget hitEffectTarget = HitEffectTarget.Enemy;
 
-	private enum ExplosionPos 
-	{
-		Projectile = 0,
-		Enemy = 1,
-		Player = 2,
-	}
+    [SerializeField] private float autoDestructionTime = 10f;
 
-	private void Start()
-	{
-		Invoke(nameof(DestroyGameObj), autoDestructionTime);
-	}
+    public bool isAreaEffect = false;
 
-	private void Update()
-	{
-		transform.Translate(Speed * Time.deltaTime * Vector3.up, Space.Self);
-	}
+	public float AreaScale = 1f;
 
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		
-		if (collision.CompareTag("Enemy"))
-		{
-			if (!isAreaEffect)
-			{
-				SpellDamageDealer.Deal(ElementType, collision.gameObject, Damage);
-				if (explosionPosition == ExplosionPos.Enemy)
-				{
-					Utils.GetExplosionManager().InitCollisionEffect(ElementType, collision.transform.position);
-				}
-				else if (explosionPosition == ExplosionPos.Projectile)
-				{
-					Utils.GetExplosionManager().InitCollisionEffect(ElementType, transform.position);
-				}
-				else if (explosionPosition == ExplosionPos.Player) 
-				{
-					Utils.GetExplosionManager().InitCollisionEffect(ElementType, Utils.GetPlayerObject().transform.position);
-				}
-				if (!HasNoPenetrateLlimit)
-				{
-					PenetrateTime--;
-					if (PenetrateTime < 0)
-					{
-						GetComponent<Collider2D>().enabled = false;
-						DestroyGameObj();
-					}
-				}
-			}
-			else 
-			{
-				// Init explosion here
-			}
+    private enum HitEffectTarget
+    {
+        Projectile = 0,
+        Enemy = 1,
+        Player = 2,
+    }
 
-		}
-	}
+    private void Start()
+    {
+        Invoke(nameof(DestroyGameObj), autoDestructionTime);
+    }
 
-	private void DestroyGameObj()
-	{
-		Destroy(gameObject);
-	}
+    private void Update()
+    {
+        transform.Translate(Speed * Time.deltaTime * Vector3.up, Space.Self);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (!collision.CompareTag("Enemy")) return;
+
+        if (!isAreaEffect)
+        {
+            SpellDamageDealer.Deal(ElementType, collision.gameObject, CollisionDamage);
+            if (hitEffectTarget == HitEffectTarget.Enemy)
+            {
+                Utils.GetExplosionManager().InitCollisionEffect(ElementType, collision.transform.position);
+            }
+            else if (hitEffectTarget == HitEffectTarget.Projectile)
+            {
+                Utils.GetExplosionManager().InitCollisionEffect(ElementType, transform.position);
+            }
+            else if (hitEffectTarget == HitEffectTarget.Player)
+            {
+                Utils.GetExplosionManager().InitCollisionEffect(ElementType, Utils.GetPlayerObject().transform.position);
+            }
+            if (HasPenetrateLlimit)
+            {
+                PenetrateTime--;
+                if (PenetrateTime < 0)
+                {
+                    GetComponent<Collider2D>().enabled = false;
+                    DestroyGameObj();
+                }
+            }
+        }
+        else
+        {
+			if (HasPenetrateLlimit)
+            {
+                PenetrateTime--;
+                if (PenetrateTime < 0)
+                {
+                    GetComponent<Collider2D>().enabled = false;
+                    DestroyGameObj();
+                }
+            }
+            GenerateExplosion(transform, AreaScale);
+        }
+
+    }
+
+    private void GenerateExplosion(Transform target, float scale)
+    {
+        GameObject explosion = Instantiate(AreaEffectPrefab, target.position, Quaternion.identity);
+        ExplosionController explosionController = explosion.GetComponent<ExplosionController>();
+        explosionController.ElementType = ElementType;
+        explosionController.Scale = scale;
+    }
+
+    private void DestroyGameObj()
+    {
+        Destroy(gameObject);
+    }
 }
