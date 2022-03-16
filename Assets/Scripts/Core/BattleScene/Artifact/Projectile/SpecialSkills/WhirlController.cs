9120e7eb1422979441a75796e4e54e835174ca92
Wind_07 <sunchen5575@126.com>
2022-03-16 19:44:30 +0100

    Update WhirlController

--- Assets/Scripts/Core/BattleScene/Artifact/Projectile/SpecialSkills/WhirlController.cs
+++ Assets/Scripts/Core/BattleScene/Artifact/Projectile/SpecialSkills/WhirlController.cs
@@ -5,43 +5,53 @@ using UnityEngine;
 public class WhirlController : MonoBehaviour
 {
     [SerializeField] private float rotateSpeed = 120f;
-    [SerializeField] private float expandSpeed = 2f;
-    [SerializeField] private float lastTime = 2f;
-    [SerializeField] private float forceFactor = 40f;
+    [SerializeField] private float finalScale = 4f;
 
-    private bool isDisappear = false;
+    [SerializeField] private float expandTime = 2f;
+    [SerializeField] private float forceFactor = 40f;
+    [SerializeField] private float disappearTIme = 0.3f;
+
+    private float expandSpeed;
+    private float disappearSpeed;
+
     private void Start()
     {
-        Invoke(nameof(DestroyObject), lastTime);
+        transform.localScale = Vector3.zero;
+        expandSpeed = finalScale / expandTime;
+        disappearSpeed = finalScale / disappearTIme;
     }
 
     private void Update()
     {
-        if (!isDisappear)
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * rotateSpeed));
+        if (expandTime > 0)
         {
-            transform.localScale = transform.localScale + Time.deltaTime * new Vector3(1, 1, 0) * expandSpeed;
-            transform.Rotate(new Vector3(0, 0, Time.deltaTime * rotateSpeed));
+            transform.localScale += Time.deltaTime * Vector3.one * expandSpeed;
+            expandTime -= Time.deltaTime;
         }
-    }
+        else if (expandTime <= 0 && disappearTIme > 0)
+        {
+            transform.localScale -= Time.deltaTime * Vector3.one * disappearSpeed;
+            disappearTIme -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
 
-
-    private void StartDisappear()
-    {
-        isDisappear = true;
     }
-    private void DestroyObject()
-    {
-        Destroy(gameObject);
-    }
-
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
+        if (expandTime > 0)
         {
-            GameObject enemy = collision.gameObject;
-            Vector2 force = (enemy.transform.position - transform.position).normalized * forceFactor;
-            enemy.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Force);
+            if (collision.CompareTag("Enemy"))
+            {
+                GameObject enemy = collision.gameObject;
+                Vector2 force = (enemy.transform.position - transform.position).normalized * forceFactor;
+                enemy.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Force);
+            }
+
         }
     }
 
